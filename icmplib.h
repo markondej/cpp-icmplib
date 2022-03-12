bd887077cdc6118d6d30a5ce52b43d3a5b22203e
Tobias <tobiasstein1@gmx.net>
2022-03-12 23:14:40 +0100

    changed ping function to static
    This fixes a linker issue when including the header in multiple files

--- icmplib.h
+++ icmplib.h
@@ -621,7 +621,7 @@ namespace icmplib {
     using PingResult = ICMPEcho::Result;
     using PingResponseType = ICMPEcho::Result::ResponseType;
 
-    PingResult Ping(const IPAddress &target, unsigned timeout = 60, uint16_t sequence = 1, uint8_t ttl = 255) {
+    static PingResult Ping(const IPAddress &target, unsigned timeout = 60, uint16_t sequence = 1, uint8_t ttl = 255) {
         return ICMPEcho::Execute(target, timeout, sequence, ttl);
     }
 }
